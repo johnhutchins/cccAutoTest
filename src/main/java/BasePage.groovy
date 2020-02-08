3988d46b51a989b1fc694403e7066b1c4775e80b
John Hutchins <johnwallshutchins@gmail.com>
2020-02-07 22:51:03 -0500

    initial commit

---
+++ src/main/java/BasePage.groovy
@@ -0,0 +1,85 @@
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.interactions.Action
+import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+
+class BasePage {
+    private final String BASE_URL = 'https://www.callawaycloud.com'
+    private final String HOME_LINK = '//*[@id="nav"]/li[1]/a'
+    private final String ABOUT_US_LINK = '//*[@id="nav"]/li[2]/a'
+    private final String TESTIMONIALS_LINK = '//*[@id="nav"]/li[3]/a'
+    private final String TESTIMONIALS_SECTION = '#testimonials'
+    private final String TESTIMONIALS_NEXT
+    private final String TESTIMONIALS_PREVIOUS
+    private final String TESTIMONIAL_CLIENT_SECTION = '.owl-wrapper'
+    private final String TEAM_LINK = '//*[@id="nav"]/li[4]/a'
+    private final String CONTACT_US_LINK = '//*[@id="nav"]/li[5]/a'
+    private final String CONTACT_FORM = '.contact-form'
+    private final String STAT_COUNTER = '.stat-counter'
+    private final String PHONE_LINK = '//*[@id="give-us-a-shout"]/div/div[1]/div/div[2]/a'
+
+
+    private final String FORM_NAME_INPUT = '//*[@id="wpcf7-f49-o1"]/form/div[2]/span[2]/span/input'
+    private final String FORM_EMAIL_INPUT = '//*[@id="wpcf7-f49-o1"]/form/div[3]/span[2]/span/input'
+    private final String FORM_TEXTAREA_INPUT = '//*[@id="wpcf7-f49-o1"]/form/p[1]/span[2]/span/textarea'
+
+    private WebDriver driver
+
+    public BasePage(WebDriver driver){
+        driver.get(BASE_URL)
+        this.driver = driver
+    }
+
+    public String getPageHeader(){
+        WebDriverWait wait = new WebDriverWait(driver, 5)
+        WebElement s =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(HEADER_H1)))
+        return s.getText()
+    }
+
+    public void clickOnSection(){
+        driver.findElement(By.xpath(TEAM_LINK)).click()
+    }
+
+    public String getPhoneNumber(){
+        WebDriverWait wait = new WebDriverWait(driver, 5)
+        WebElement s =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PHONE_LINK)))
+        return s.getText()
+    }
+
+    public void clickEmail(){
+
+    }
+
+    public void clickMap(){
+
+    }
+
+    public void sendEmailFromContactForm(){
+
+    }
+
+    public List<WebElement> returnContactFormInputs(){
+        WebDriverWait wait = new WebDriverWait(driver, 5)
+        List<WebElement> contactEls = []
+        //click on the contact us link
+        driver.findElement(By.xpath(CONTACT_US_LINK)).click()
+        WebElement formName =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(FORM_NAME_INPUT)))
+        WebElement formEmail =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(FORM_EMAIL_INPUT)))
+        WebElement formTextArea =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(FORM_TEXTAREA_INPUT)))
+        contactEls.add(formName)
+        contactEls.add(formEmail)
+        contactEls.add(formTextArea)
+        return contactEls
+    }
+
+    public String getStatCounterValues(){
+        WebDriverWait wait = new WebDriverWait(driver, 5)
+        WebElement s =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(STAT_COUNTER)))
+        System.out.println(s)
+        return s
+    }
+}
+
---
+++ src/main/java/BasePageTest.groovy
@@ -0,0 +1,53 @@
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.firefox.FirefoxDriver
+
+class BasePageTest {
+    //vars
+    private WebDriver ff
+
+    @Before
+    public void openBrowser(){
+        ff = new FirefoxDriver()
+    }
+
+    @After
+    public void closeBrowser(){
+        ff.quit()
+    }
+
+    @Test
+    public void clickTeamSection(){
+        System.out.println("this should click the team section")
+    }
+
+    @Test
+    public void ensureCorrectPhoneNumber(){
+        BasePage basePage = new BasePage(ff)
+        assert basePage.getPhoneNumber() == '307.200.7224'
+    }
+
+    //needs refactor, but quick smoke test
+    @Test
+    public void returnContacFormInputs(){
+        BasePage basePage = new BasePage(ff)
+        List<WebElement> contactElements = basePage.returnContactFormInputs()
+//        contactElements[1].click()
+//        sleep(5000)
+        //click an input, then enter some data
+
+
+
+        assert contactElements.size() == 3
+    }
+
+    @Test
+    public void ensureStatCounterHasValue(){
+        BasePage basePage = new BasePage(ff)
+        basePage.getStatCounterValues()
+    }
+
+}
